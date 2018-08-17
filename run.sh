fcd96e9aec17c4b9bf82fa82c32170508263fa14
Richard White <r@rwhite.no>
2018-08-17 08:56:47 +0200

    x

--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-if [[ "$CI_PWD" != "" ]]; then
+if [[ "$CI_PWD_OUTSIDE" != "" ]]; then
+echo "$CI_PWD_OUTSIDE"
 echo 1
+
   docker run --rm \
   -u="rstudio" \
-  -v $CI_PWD/:/git/ \
+  -v $CI_PWD_OUTSIDE/:/git/ \
   raw996/dhadley:3.4.3 ls /git/
 echo 1
+
+  docker run --rm \
+  -u="rstudio" \
+  -v $CI_PWD_OUTSIDE/:/git/ \
+  raw996/dhadley:3.4.3 Rscript /git/fhi-infectious.github.io/run.R
+
 else
   echo "hello"
 fi
