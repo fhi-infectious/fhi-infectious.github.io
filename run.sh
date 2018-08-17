8075c14b0e2a6597dc671556c919789c6a1caaa7
Richard White <r@rwhite.no>
2018-08-17 08:51:57 +0200

    ls

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ if [[ "$CI_PWD" != "" ]]; then
   docker run --rm \
   -u="rstudio" \
   -v $CI_PWD/:/git/ \
+  raw996/dhadley:3.4.3 ls /git/
+
+  docker run --rm \
+  -u="rstudio" \
+  -v $CI_PWD/:/git/ \
   raw996/dhadley:3.4.3 Rscript /git/fhi-infectious.github.io/run.R
 else
   echo "hello"
