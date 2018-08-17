45f8972c17e6a3d4c520e54d410b1c6bc5ad6686
Richard White <r@rwhite.no>
2018-08-17 08:54:00 +0200

    test

--- run copy.sh
+++ run copy.sh
@@ -7,6 +7,10 @@ if [[ "$CI_PWD" != "" ]]; then
   -v $CI_PWD/:/git/ \
   raw996/dhadley:3.4.3 ls /git/
 
+  docker run --rm \
+  -u="rstudio" \
+  -v $CI_PWD/:/git/ \
+  raw996/dhadley:3.4.3 Rscript /git/fhi-infectious.github.io/run.R
 else
   echo "hello"
 fi
