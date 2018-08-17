123f7fad901ea416a2d62b666a1081ec94291472
Richard White <r@rwhite.no>
2018-08-17 09:23:44 +0200

    test

--- bin/run.sh
+++ bin/run.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 if [[ "$CI_PWD_OUTSIDE" != "" ]]; then
-echo "$CI_PWD_OUTSIDE"
-echo 1
-
-  docker run --rm \
-  -u="rstudio" \
-  -v $CI_PWD_OUTSIDE/:/git/ \
-  raw996/dhadley:3.4.3 ls /git/
-echo 1
-
   docker run --rm \
   -u="rstudio" \
   -v $CI_PWD_OUTSIDE/:/git/ \
   raw996/dhadley:3.4.3 Rscript /git/fhi-infectious.github.io/run.R
 
+  git config --global user.email "xx@fhi.no"
+  git config //global user.name "FHI Jenkins"
 else
   echo "hello"
 fi
