e25c316c4d464604f78a163f7e8d98243aa36364
Richard White <r@rwhite.no>
2018-08-17 08:54:46 +0200

    x

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ "$CI_PWD" != "" ]]; then
-
+echo 1
   docker run --rm \
   -u="rstudio" \
   -v $CI_PWD/:/git/ \
   raw996/dhadley:3.4.3 ls /git/
-
+echo 1
 else
   echo "hello"
 fi
