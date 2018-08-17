c06099cae37db2a56b2ceafd1b17663034d97b11
Richard White <r@rwhite.no>
2018-08-17 09:44:05 +0200

    x

--- bin/git.sh
+++ bin/git.sh
@@ -2,8 +2,8 @@
 
 if [[ "$CI_PWD" != "" ]]; then
   cd $CI_PWD/fhi-infectious.github.io
-  git config user.email "xx@fhi.no"
-  git config user.name "FHI Jenkins"
+  git config user.email xx@fhi.no
+  git config user.name raubreywhite
 
   git checkout -b master
   git add .
